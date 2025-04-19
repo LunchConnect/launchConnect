d3ffeda0d65216ed05fef671fc48fd3fd4ef6325
blessingsunday1995 <blessing.sunday38@yahoo.com>
2025-04-19 11:06:09 +0100

    profile update

--- utils/axiosInstance.ts
+++ utils/axiosInstance.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://launch-connects.onrender.com/api";
+const BASE_URL = "http://89.117.36.172:3000/api";
 
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
