cf46b81baa2ed7edb4a26a7e5d386b55e953a423
Pedrosaint <chideranwaolisa@gmail.com>
2025-05-01 16:22:37 +0100

    bugfix/next.config.js

--- next.config.js
+++ next.config.js
@@ -6,14 +6,14 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination: "http://89.117.36.172:3010/api/:path*", // Proxy to backend
-      },
-    ];
-  },
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: "/api/:path*",
+  //       destination: "http://89.117.36.172:3010/api/:path*", // Proxy to backend
+  //     },
+  //   ];
+  // },
 };
 
 module.exports = nextConfig;
--- utils/axiosInstance.ts
+++ utils/axiosInstance.ts
@@ -3,7 +3,7 @@
 import axios from "axios";
 
 // 👇 Use relative baseURL to let Next.js proxy handle it
-const BASE_URL = "/api";
+const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
